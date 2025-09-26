60f60eac5f1eb05a8737d5fc6cfad6f23ccb175d
ProBotiX Bladel <probotixbladel@users.noreply.github.com>
2025-09-26 16:29:05 +0200

    Adding AprilTag detection with custom tag library

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rookie_Tank.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rookie_Tank.java
@@ -50,7 +50,7 @@ import com.qualcomm.robotcore.util.Range;
  */
 
 @TeleOp(name="Robot: Teleop Tank", group="Robot")
-//@Disabled
+@Disabled
 public class Rookie_Tank extends OpMode{
 
     /* Declare OpMode members. */
